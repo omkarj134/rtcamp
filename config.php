3c51514efafd66c3dacd4a7c9d0f57edf80794d2
omkarj134 <omkarj134@gmail.com>
2014-06-29 18:18:59 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define ('CONSUMER_KEY', 'h3HiGOm1iWDKHCeS233Gc2P7l');
+define ('CONSUMER_SECRET','F1nUwAFgdH4479JKYfKlKLUO7SkBy3xODqzn9aeaRTvAU8SO5I');
+define ('OAUTH_CALLBACK','http://www.omkar-joshi.in/twitter/callback.php');
+
+?>
